39e139ef634937bb009a49093a08312f3047c90d
JYULAB <89278655+JYULAB@users.noreply.github.com>
2021-08-24 09:21:15 -0500

    Add files via upload

---
+++ MA_plot_example.R
@@ -0,0 +1,42 @@
+------MA plot by log2 value add intensity of HOXB13 (m854) as color
+
+setwd("D:\\xiaodong\\2020_07_22\\plot")
+data <- read.table("HDAC3_peaks_data_for_ma_plot_tag_counts_m682_vs_m683_add_m854.txt", header=TRUE)
+#data = data[data$control>=1 & data$treatment>=1,]
+data = data[order(data$HOXB13),]   # sort by intensity of HOXB13
+a <- data$control
+b <- data$treatment
+c <- data$HOXB13
+
+x = (a+b)/2
+y = a-b
+
+#install.packages("table1")
+library("table1")  # for eqcut function
+library(xfun) # for eqcut function
+
+k = eqcut(c,20)
+
+library(RColorBrewer)
+#rbPal <- colorRampPalette(brewer.pal(20, "YlOrRd"))
+rbPal <- colorRampPalette(c("navy", "green", "yellow", "firebrick3"))
+my_color <- rbPal(20)[as.numeric(k)]
+plot(x, y, cex=0.5, xlab = "Average HDAC3 enrichment (log2)", xlim=c(0,20), ylab = "Log2 Fold HDAC3 enrichment (pGIPZ / shHoxB13)", col = my_color, pch = 8, frame = FALSE)
+#legend("topright",legend=c(seq(2,20,by=2)),col =rbPal(20),pch=8)
+abline(h=1, col="black", lty = 2)
+abline(h=-1, col="black", lty = 2)
+
+
+up = length(y[y>=1])
+down = length(y[y<=-1])
+unchanged = length(y[y<1 & y>-1])
+
+text(16, -4, up, cex = .8, col="black")
+text(16, -7, unchanged, cex = .8, col="black")
+text(16, -10, down, cex = .8, col="black")
+savePlot(filename = "HDAC3_MA_plot_m682_vs_m683_addm854", type = "pdf")
+
+legend_image <- as.raster(matrix(rev(rbPal(20)), ncol=1))
+plot(c(0,2),c(0,1),type = 'n', main="pGIPZ HOXB13 enrichment (log2)", axes = F,xlab = '', ylab = '')
+rasterImage(legend_image, 0,0,1,1)
+savePlot(filename = "HDAC3_MA_plot_m682_vs_m683_addm854_legend", type = "pdf")
