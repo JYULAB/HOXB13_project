82f2bd98ffe2baab7f758cb5d76950f3f8e82732
JYULAB <89278655+JYULAB@users.noreply.github.com>
2022-02-01 10:35:31 -0600

    Update venn_diagram.R

--- venn_diagram.R
+++ venn_diagram.R
@@ -1,7 +1,6 @@
-setwd("D:\\sarki\\han")
 library(Vennerable)
 
-venn<- read.table("GSM935508vsGSM935408vsds015.txt", header = TRUE, sep = "\t")
+venn<- read.table("./input/m854vsm442vsm682.txt", header = TRUE, sep = "\t")
 c =   venn[1,4]
 b =   venn[2,4]
 cb =  venn[3,4]
@@ -10,6 +9,6 @@ ca =  venn[5,4]
 ab =  venn[6,4]
 abc = venn[7,4]
 
-Vdemo3 <- Venn(SetNames = c("a", "b", "c"), Weight = c(0,a,b,ab,c,ca,cb,abc))
+Vdemo3 <- Venn(SetNames = c("HOXB13", "AR", "HDAC3"), Weight = c(0,a,b,ab,c,ca,cb,abc))
 plot(Vdemo3, doWeights = TRUE)
-savePlot(filename = "xxxxxxx overlapping", type = "pdf")
+savePlot(filename = "./result/m854vsm442vsm682 overlapping", type = "pdf")
