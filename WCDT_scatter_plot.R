60e70b425da6e8c33aff2118c1ecd1df772fa932
JYULAB <89278655+JYULAB@users.noreply.github.com>
2022-02-01 10:12:48 -0600

    Update WCDT_scatter_plot.R

--- WCDT_scatter_plot.R
+++ WCDT_scatter_plot.R
@@ -1,24 +1,19 @@
 library(scales)
 
-setwd("C:\\work\\xiaodong\\2021_05_15\\result_08_09_21")    
-
 returns <- runif(100)
 yticks_val <- pretty_breaks(n=10)(returns)
 
-a1 <- scan("high.txt")
+a1 <- scan("./input/high18.txt")
 a2 <- matrix(a1,ncol=101,byrow=T)
-a3 <- apply(a2[2:50,],2,mean, na.rm=TRUE)
-b1 <- scan("low.txt")
+a3 <- apply(a2[2:19,],2,mean, na.rm=TRUE)
+b1 <- scan("./input/low18.txt")
 b2 <- matrix(b1,ncol=101,byrow=T)
-b3 <- apply(b2[2:50,],2,mean, na.rm=TRUE)
+b3 <- apply(b2[2:19,],2,mean, na.rm=TRUE)
+d1 <- scan("./input/tumor.txt")
+d2 <- matrix(d1,ncol=101,byrow=T)
+d3 <- apply(d2[2:6,],2,mean, na.rm=TRUE)
 
-
-plot(cbind(a2[1,]/1000,a3),type="h",xlab="HOXB13 location (kb) at Chromosome 17",ylim=c(0,1),ylab="Percentage methylation",col=rgb(red = 1, green = 0, blue = 0, alpha = 1),pch = 0, lwd = 3, cex = 8, yaxt="n")
-points(cbind(b2[1,]/1000,b3),type="h",col = rgb(red = 0, green = 0, blue = 1, alpha = 0.2), pch = 0, lwd = 3, cex = 8)
-#plot(cbind(b2[1,]/1000,b3),type="p",xlab="HOXB13 location (kb) at Chromosome 17",ylim=c(0,1),ylab="Percentage methylation",col=rgb(red = 0, green = 0, blue = 1, alpha = 1),pch = 16, lwd = 3, cex = 1, yaxt="n")
-#points(cbind(a2[1,]/1000,a3),type="p",col = rgb(red = 1, green = 0, blue = 0, alpha = 1), pch = 16, lwd = 3, cex = 1)
-legend('topright', c("HOXB13 low", "HOXB13 high"), lty=1, col=c('blue', 'red'), bty='n', cex=1, lwd = 3)
-axis(2, at=yticks_val, lab=percent(yticks_val, trim=FALSE, scale = 100, accuracy=1))
-abline(v=48728750/1000, col="orange", lty=2)  ## TSS
-abline(v=48724763/1000, col="black", lty=2)   ## TES
-text(locator(), labels = c("TSS", "TES"))
+plot(cbind(b2[1,]/1000,b3),type="p",xlab="HOXB13 location (kb) at Chromosome 17",ylim=c(0,1),ylab="Percentage methylation",col= 'blue', pch = 16, lwd = 3, cex = 1, yaxt="n")
+points(cbind(a2[1,]/1000,a3),type="p",col = 'red', pch = 16, lwd = 3, cex = 1)
+points(cbind(d2[1,]/1000,d3),type="p",col = 'darkolivegreen', pch = 16, lwd = 3, cex = 1)
+legend('topright', c("tumor", "HOXB13 low CRPC", "HOXB13 high CRPC"), lty=1, col=c('darkolivegreen', 'blue', 'red'), bty='n', cex=1, lwd = 3)
